671dce595b37b97fa7e662b4c644be801088974d
Kateryna Verveda <katushka211@gmail.com>
2023-06-05 20:20:29 +0200

    update filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,33 +3,16 @@ import { Layout } from './Layout/Layout';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-// import { nanoid } from 'nanoid';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContact } from 'redux/contacts/slice';
-import { changeFilter } from 'redux/filter/slice';
 
 export const App = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts.initialContacts);
   const filter = useSelector(state => state.filter);
 
-  // const addContactHandler = (contact, name) => {
-  //   if (contacts.find(contact => contact.name === name)) {
-  //     alert(`${name} is already in contacts`);
-  //     return;
-  //   } else {
-  //     const newContact = { ...contact, id: nanoid() };
-
-  //     dispatch(addContact(newContact));
-  //   }
-  // };
-
   const deleteContactHandler = contactID => {
     dispatch(deleteContact(contactID));
-  };
-
-  const changeFilterHandler = event => {
-    dispatch(changeFilter(event.currentTarget.value));
   };
 
   const getVisiblesContacts = () => {
@@ -46,7 +29,7 @@ export const App = () => {
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-      <Filter value={filter} onChange={changeFilterHandler} />
+      <Filter />
       <ContactList items={visibleContacts} onDelete={deleteContactHandler} />
       <GlobalStyle />
     </Layout>
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,16 @@
-import PropTypes from 'prop-types';
-export const Filter = ({ value, onChange }) => {
+import { getFilter } from 'redux/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter } from 'redux/filter/slice';
+export const Filter = () => {
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+  const changeFilterHandler = event => {
+    dispatch(changeFilter(event.currentTarget.value));
+  };
   return (
     <label>
       Find contacts by name
-      <input type="text" value={value} onChange={onChange} />
+      <input type="text" value={filter} onChange={changeFilterHandler} />
     </label>
   );
-};
-
-Filter.propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
 };
