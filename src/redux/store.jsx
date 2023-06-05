a7493bdeb450318593fae1ba69b2e8e5697b4cb4
Kateryna Verveda <katushka211@gmail.com>
2023-06-05 14:03:15 +0200

    add persistor

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactSlice } from './contacts/slice';
 import { filterSlice } from './filter/slice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedContactsReducer = persistReducer(
+  persistConfig,
+  contactSlice.reducer
+);
+
 export const store = configureStore({
   reducer: {
-    contacts: contactSlice.reducer,
+    contacts: persistedContactsReducer,
     filter: filterSlice.reducer,
   },
 });
+
+export const persistor = persistStore(store);
