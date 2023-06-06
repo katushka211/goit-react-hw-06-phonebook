2d0cae628a3173217290ccd2051e4428cea32de9
Kateryna Verveda <katushka211@gmail.com>
2023-06-06 10:50:15 +0200

    update selector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Contact } from '../Contact/Contact';
-import { getContacts, getFilter } from 'redux/selectors';
+import { getVisibleContacts } from 'redux/selectors';
 
-export const ContactList = ({ items, onDelete }) => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-  const getVisiblesContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  const visibleContacts = getVisiblesContacts();
+export const ContactList = () => {
+  const visibleContacts = useSelector(getVisibleContacts);
 
   return (
     <ul>
